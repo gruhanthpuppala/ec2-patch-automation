c9d3feeffde4dd000f2e414d54fa5576fda4bbcf
gruhanthpuppala <gruhanth.puppala@gmail.com>
2025-05-12 15:01:53 +0530

    added error handling menthods throught the stages

--- pipeline_script.groovy
+++ pipeline_script.groovy
@@ -1,56 +1,95 @@
 pipeline {
     agent any
     stages {
-        stage('Fetch instances') {
+        stage('AWS env setup & Fetch Instance details') {
             steps {
                 echo "========================================================================================================"
                 echo 'Logging into AWS CLI'
                 script {
-                    withCredentials([aws(credentialsId: 'awscli_access_secrets', variablePrefix: 'AWS')]) {
-                        sh 'aws sts get-caller-identity'
-                        echo "Login sucessful"
-                        echo 'Collecting the data of running instances from AWS env'
-                        sh '''
-                        aws ec2 describe-instances \\
-                        --filters "Name=tag:Project,Values=SSH-AUTO" \\
-                        "Name=instance-state-name,Values=running" \\
-                        --query "Reservations[*].Instances[*].[PublicIpAddress]" \\
-                        --output text > public_ips.txt
-                        '''
-                        echo "Output generated to public_ips.txt file"
-                        sh 'cat public_ips.txt'
-                    }
-                    
+                    try {
+                        echo "Fetching creds from the secrets"
+                        withCredentials([aws(credentialsId: 'awscli_access_secrets', variablePrefix: 'AWS')]) {
+                            echo "Setting AWS ENV..."
+                            sh 'aws sts get-caller-identity'
+                            echo "Login sucessful" 
+                            echo "Fetching running instances for the TAG Project=SSH-AUTO"
+                            // future update: we can bring a feature to this pipeline to give a user input to fetch the users choice of TAG
+                            // so this can be use to fetch instances of multiple tags to perform package updates  
+                            sh '''
+                            aws ec2 describe-instances \\
+                            --filters "Name=tag:Project,Values=SSH-AUTO" \\
+                            "Name=instance-state-name,Values=running" \\
+                            --query "Reservations[*].Instances[*].[PublicIpAddress]" \\
+                            --output text > public_ips.txt
+                            '''
+                        }
+                    } catch (e) {
+                        echo "Error occured with exit code:Error while fetching the Instance list for TAG Project=SSH-AUTError while fetching the Instance list for TAG Project=SSH-AUT ${e.getMessage()}"
+                        error("Halting pipeline; No running instances found / AWS env config issue.")
+                        }
                 }
-                echo "======================================================================================================"
+                echo ("Collecting the data required.....")
+                echo "========================================================================================================"
             }
         }
-        stage('SSH into instances') {
+        stage('Data Existance Check') {
             steps {
-                echo "======================================================================================================"
+                echo "========================================================================================================"
                 script {
+                    try {
+                        def status = sh(script: 'test -f public_ips.txt', returnStatus: true)
+                        if (status == 0) {
+                            echo "Data exist and ready to update the packages"
+                        } 
+                        else {
+                            echo "No file/data found. Exiting....."
+                            currentBuild.result = 'FAILURE'
+                            return
+                        }
+                    } catch (e) {
+                        currentBuild.result = 'FAILURE'
+                        echo "No file found: ${e.getMessage()}"
+                    }
+                }
+                echo "========================================================================================================"
+            }
+        }
+        stage('Login into instance to update packages') {
+            steps {
+                echo "========================================================================================================"
+                script {
+
                     def ipList = readFile('public_ips.txt').split(/\s+/).findAll { it }
+
                     if (ipList.isEmpty()) {
-                        echo "No IPs found. Skipping SSH step."
+                        echo "File exist but No IPs found. Skipping SSH step."
                         return
                     }
-                    echo "Found ${ipList.size()} IP(s) to SSH into..."
+
+                    echo "Found ${ipList.size()} IP(s), performing SSH into each and update the packages"
 
                     sshagent(['private_keys']) {
-                        ipList.each { 
-                            ip -> echo ">>> Connecting to ${ip}..."
+                        ipList.each {
+                            ip -> echo "Trying to connect ${ip}"
                             try {
-                                sh "ssh -o StrictHostKeyChecking=no -o BatchMode=yes ec2-user@${ip} 'sudo yum update -y'"
+                                def status = sh(
+                                    script: "ssh -o StrictHostKeyChecking=no -o BatchMode=yes ec2-user@${ip} 'sudo yum update -y'",
+                                    returnStatus: true
+                                )
+                                if (status == 0) {
+                                    echo "Connected to ${ip} and performed the update."
+                                    echo "Update completed"
+                                } else {
+                                    echo "Unable to connect to ${ip}, SSH Error with exit code: ${status} "
+                                }
                             } catch (e) {
                                 echo ">>> SSH failed on ${ip}: ${e.getMessage()}"
                             }
-                            }
                         }
                     }
-                echo "======================================================================================================"
                 }
+                echo "========================================================================================================"
             }
         }
-        
     }
-
+}
